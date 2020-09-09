8579104732066c69ef94463907c88b07b68230e6
andresGuaman <andres.guaman.est@tecazuay.edu.ec>
2020-09-09 15:30:49 -0500

    Proyecto-Spring-final
    Proyecto Spring para M&W

---
+++ springMyStore/src/main/java/com/example/springMyStore/SpringMyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springMyStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyStoreApplication.class, args);
+	}
+
+}
---
+++ springMyStore/src/test/java/com/example/springMyStore/SpringMyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMyStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
