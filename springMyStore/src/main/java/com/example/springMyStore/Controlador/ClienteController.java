974ed9bc9743916df1bf1649bfccfef74b4f98a8
Andrés <freddy.sapatanga.est@tecazuay.edu.ec>
2020-10-07 01:23:14 -0500

    Método findByUserPas creado en ClienteRepository y ClienteController

--- springMyStore/src/main/java/com/example/springMyStore/Controlador/ClienteController.java
+++ springMyStore/src/main/java/com/example/springMyStore/Controlador/ClienteController.java
@@ -3,10 +3,8 @@ package com.example.springMyStore.Controlador;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import com.example.springMyStore.Modelo.Cliente;
 import com.example.springMyStore.Repositorio.ClienteRepository;
-
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,22 +31,35 @@ public class ClienteController {
 
     @GetMapping("/Cliente")
     public List<Cliente> getAllCliete(){
+
         return clienteRepository.findAll();
     }
 
     @GetMapping("/Cliente/{cli_id}")
     public ResponseEntity<Cliente> getClieteById(@PathVariable(value = "cli_id")Long cli_id)throws ResourceNotFoundException{
+
         Cliente cliente=clienteRepository.findById(cli_id).orElseThrow(()-> new ResourceNotFoundException("No se pudo encontrar al Cliente con el id:"+cli_id));
         return ResponseEntity.ok().body(cliente);
     }
 
+    //Find by Username and password
+    @GetMapping("/Cliente/{cli_usuario}/{cli_password}")
+    public ResponseEntity<Cliente> getClieteByUserPass(@PathVariable(value = "cli_usuario") String cli_usuario, @PathVariable(value = "cli_password") String cli_password) {
+
+        Cliente cliente = clienteRepository.findByUserPass(cli_usuario, cli_password);
+        return ResponseEntity.ok().body(cliente);
+    }
+
+
     @PostMapping("/Cliente")
+
     public Cliente crearCliente(@Validated @RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     @PutMapping("Cliente/{cli_id}")
     public ResponseEntity<Cliente> updateClienteById(@PathVariable(value = "cli_id")Long cli_id, @Validated @RequestBody Cliente cliente)throws ResourceNotFoundException{
+
         Cliente cliente2=clienteRepository.findById(cli_id).orElseThrow(()-> new ResourceNotFoundException("No se pudo encontrar al Cliente con el id:"+cli_id));
         cliente2.setCli_usuario(cliente.getCli_usuario());
         cliente2.setCli_password(cliente.getCli_password());
@@ -59,6 +70,7 @@ public class ClienteController {
 
     @DeleteMapping("/Cliente/{cli_id}")
     public Map<String, Boolean>deleteCliente(@PathVariable(value = "cli_id")Long cli_id)throws ResourceNotFoundException{
+
         Cliente cliente2=clienteRepository.findById(cli_id).orElseThrow(()-> new ResourceNotFoundException("No se pudo encontrar al Cliente con el id:"+cli_id));
        clienteRepository.delete(cliente2);
        Map<String, Boolean>response=new HashMap<>();
--- springMyStore/src/main/java/com/example/springMyStore/Repositorio/ClienteRepository.java
+++ springMyStore/src/main/java/com/example/springMyStore/Repositorio/ClienteRepository.java
@@ -2,9 +2,13 @@ package com.example.springMyStore.Repositorio;
 
 import com.example.springMyStore.Modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
     
+    @Query(value = "SELECT c FROM Cliente c WHERE c.cli_usuario = :cli_usuario AND c.cli_password = :cli_password")
+    Cliente findByUserPass(@Param("cli_usuario") String cli_usuario, @Param("cli_password") String cli_password);
 }
